fd3c753f1bf0b29d96909241c1813513aa0e07c6
jihanfachrani <jihan.fachrani@unida.ac.id>
2024-04-27 10:13:12 +0700

    UTS
    mobile programming

---
+++ lib/uts_mp/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String openWeatherBaseURL = 'https://api.openweathermap.org/data/2.5';
+  static const String openWeatherApiKey = 'c0bbcfa01cdce37854ec90e40cc9ffe5';
+}
